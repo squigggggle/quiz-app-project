bef418aad2b4b2472b945e79392e8797247d258d
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2024-05-20 10:03:40 +1200

    feat: add validation to auth route file I think that makes sense???????

--- routes/v1/auth.js
+++ routes/v1/auth.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 
 import { register, login } from "../../controllers/v1/auth.js";
+import { validatePostBasicUser } from "../../middleware/validation.js";
 
 const router = Router();
 
-router.route("/register").post(register);
+router.route("/register").post(register, validatePostBasicUser);
 router.route("/login").post(login);
 
 export default router;
