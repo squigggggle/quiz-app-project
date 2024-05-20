36337521e018957e0d878d906e61688e63dccadd
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2024-05-20 14:57:07 +1200

    fix: add required to all fields with required

--- middleware/validation.js
+++ middleware/validation.js
@@ -2,43 +2,41 @@ import Joi from "joi";
 
 const validatePostBasicUser = (req, res, next) => {
     const userSchema = Joi.object({
-        firstName: Joi.string().min(2).max(50).pattern(/^[a-zA-Z]+$/).messages({
+        firstName: Joi.string().min(2).max(50).pattern(/^[a-zA-Z]+$/).required().messages({
             "string.base":"First Name should be a string",
             "string.empty":"First Name cannot be empty",
             "string.min":"First Name should have a minimum length of {#limit}",
             "string.max":"First Name should have a maximum length of {#limit}",
             "string.pattern":"First Name should contain alpha characters only"
         }),
-        lastName: Joi.string().min(2).max(50).pattern(/^[a-zA-Z]+$/).messages({
+        lastName: Joi.string().min(2).max(50).pattern(/^[a-zA-Z]+$/).required().messages({
             "string.base":"Last Name should be a string",
             "string.empty":"Last Name cannot be empty",
             "string.min":"Last Name should have a minimum length of {#limit}",
             "string.max":"Last Name should have a maximum length of {#limit}",
             "string.pattern":"Last Name should contain alpha characters only"
         }),
-        username: Joi.string().unique().min(5).max(10).alphanum().messages({
+        username: Joi.string().min(5).max(10).alphanum().required().messages({
             "string.base":"Username should be a string",
             "string.empty":"Username cannot be empty",
-            "string.unique":"Username should be unique",
             "string.min":"Username should have a minimum length of {#limit}",
             "string.max":"Username should have a maximum length of {#limit}",
             "string.alphanum":"Username should contain alphanumeric characters only"
         }),
-        email: Joi.string().email().unique().ref('username').messages({
+        email: Joi.string().email().required().messages({
             "string.base":"Email should be a string",
             "string.empty":"Email cannot be empty",
             "string.email":"Email should use valid syntax",
-            "string.unique":"Email should be unique",
         }),
         // not stolen from here https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
-        password: Joi.string.min(8).max(16).pattern("^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,16}$").messages({
+        password: Joi.string.min(8).max(16).pattern("^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,16}$").required().messages({
             "string.base":"Password should be a string",
             "string.empty":"Password cannot be empty",
             "string.min":"Password should have a minimum length of {#limit}",
             "string.max":"Password should have a maximum length of {#limit}",
             "string.pattern":"Password must have at least one letter, one number and one special character"
         }),
-        confirmPassword: Joi.string.ref('password').messages({
+        confirmPassword: Joi.string.ref('password').required().messages({
             "string.base":"Confirm Password should be a string",
             "string.empty":"Confirm Password cannot be empty",
             "string.ref":"Password must match"
