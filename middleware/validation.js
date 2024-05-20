c012387afced56326cd51fb45ecccac7599385c5
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2024-05-21 11:03:06 +1200

    clean up extra commented out code

--- middleware/validation.js
+++ middleware/validation.js
@@ -36,6 +36,7 @@ const validatePostBasicUser = (req, res, next) => {
             "string.max":"Password should have a maximum length of {#limit}",
             "string.pattern.base":"Password must have at least one letter, one number and one special character"
         }),
+        // also not stolen from here https://stackoverflow.com/questions/29827082/hapi-route-joi-validation-of-password-confirmation
         confirmPassword: Joi.any().valid(Joi.ref('password')).required().messages({
             "any.only":"Password must match"
         }),
@@ -52,47 +53,3 @@ const validatePostBasicUser = (req, res, next) => {
     next();
 };
 export {validatePostBasicUser};
-
-// const validatePostBasicUser = (req, res, next) => {
-//   const institutionSchema = Joi.object({
-//     firstName: Joi.string().min(3).max(100).required().messages({
-//       "string.base": "name should be a string",
-//       "string.empty": "name cannot be empty",
-//       "string.min": "name should have a minimum length of {#limit}",
-//       "string.max": "name should have a maximum length of {#limit}",
-//       "any.required": "name is required",
-//     }),
-//     lastName: Joi.string().min(3).max(100).required().messages({
-//         "string.base": "name should be a string",
-//         "string.empty": "name cannot be empty",
-//         "string.min": "name should have a minimum length of {#limit}",
-//         "string.max": "name should have a maximum length of {#limit}",
-//         "any.required": "name is required",
-//       }),
-//     email: Joi.string().email().required().messages({
-//         "string.base":"Email should be a string",
-//         "string.empty":"Email cannot be empty",
-//         "string.email":"Email should use valid syntax",
-//     }),
-//     password: Joi.string().min(8).max(16).pattern("^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,16}$").required().messages({
-//         "string.base":"Password should be a string",
-//         "string.empty":"Password cannot be empty",
-//         "string.min":"Password should have a minimum length of {#limit}",
-//         "string.max":"Password should have a maximum length of {#limit}",
-//         "string.pattern":"Password must have at least one letter, one number and one special character"
-//     }),
-
-//   });
-
-//   const { error } = institutionSchema.validate(req.body);
-
-//   if (error) {
-//     return res.status(400).json({
-//       msg: error.details[0].message,
-//     });
-//   }
-
-//   next();
-// };
-
-// export { validatePostBasicUser };
