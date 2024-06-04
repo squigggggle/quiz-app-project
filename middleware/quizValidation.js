46922012d9ca87df0d4628fcbed749ae561b79f1
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2024-06-04 12:26:16 +1200

    feat: add error catch to quiz validation

--- middleware/quizValidation.js
+++ middleware/quizValidation.js
@@ -1,18 +1,30 @@
 import Joi from "joi";
 
 const validatePostQuiz = (req, res, next) => {
-    const quizSchema = Joi.object({
-        name: Joi.string().messages({
+  const quizSchema = Joi.object({
+    name: Joi.string().messages({
 
-        }),
-        startDate: Joi.date().messages({
+    }),
+    startDate: Joi.date().messages({
 
-        }),
-        endDate: Joi.date().messages({
+    }),
+    endDate: Joi.date().messages({
 
-        }),
-        questions: Joi.array.messages({
+    }),
+    questions: Joi.array.messages({
+        
+    }),
+  });
 
-        }),
-    })
-}
+  const { error } = userSchema.validate(req.body);
+
+  if (error) {
+    return res.status(400).json({
+      msg: error.details[0].message,
+    });
+  }
+
+  next();
+};
+
+export { validatePostQuiz };
