473d6b66defd64d209b7dfab421902360a6e4eec
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2024-06-04 16:04:11 +1200

    feat: add messages to name

--- middleware/quizValidation.js
+++ middleware/quizValidation.js
@@ -2,8 +2,12 @@ import Joi from "joi";
 
 const validatePostQuiz = (req, res, next) => {
   const quizSchema = Joi.object({
-    name: Joi.string().messages({
-
+    name: Joi.string().min(5).max(30).pattern(/^[a-zA-Z]+$/).messages({
+        "string.base":"Name should be a string",
+        "string.empty":"Name cannot be empty",
+        "string.min":"Name should have a minimum length of {#limit}",
+        "string.max":"Name should have a maximum length of {#limit}",
+        "string.pattern":"Name should contain alpha characters only",
     }),
     startDate: Joi.date().messages({
 
@@ -11,7 +15,7 @@ const validatePostQuiz = (req, res, next) => {
     endDate: Joi.date().messages({
 
     }),
-    questions: Joi.array().messages({
+    questions: Joi.array().length(10).messages({
 
     }),
   });
