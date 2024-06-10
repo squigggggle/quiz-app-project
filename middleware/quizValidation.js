bbbb916e8410460161da15092d71e866379ad5fe
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2024-06-10 14:20:27 +1200

    feat: add date valdidation

--- middleware/quizValidation.js
+++ middleware/quizValidation.js
@@ -2,6 +2,9 @@ import Joi from "joi";
 
 const validatePostQuiz = (req, res, next) => {
   const quizSchema = Joi.object({
+    categoryId: Joi.number(),
+    difficulty: Joi.string(),
+    type: Joi.string(),
     name: Joi.string().min(5).max(30).pattern(/^[a-zA-Z]+$/).messages({
         "string.base":"Name should be a string",
         "string.empty":"Name cannot be empty",
@@ -9,14 +12,18 @@ const validatePostQuiz = (req, res, next) => {
         "string.max":"Name should have a maximum length of {#limit}",
         "string.pattern":"Name should contain alpha characters only",
     }),
-    startDate: Joi.date().messages({
-
+    startDate: Joi.date().min('now').messages({
+        "date.base":"Start date should be a valid date",
+        "date.min":"Start date should be today or in the future"
     }),
-    endDate: Joi.date().messages({
-
+    endDate: Joi.date().min(Joi.ref("startDate")).max(Joi.ref("startDate", {
+      adjust: (v) => v.getTime() + (5 * 24 * 60 * 60 * 1000),})).messages({
+        "date.base":"End date should be a valid date",
+        "date.min":"End date should be after the start date",
+        "date.max":"End date should not be more than 5 days from the start date"
     }),
     questions: Joi.array().length(10).messages({
-
+      "array.length": "Questions array should contain exactly {#limit} questions",
     }),
   });
 
