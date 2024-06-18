b2fa77a9e8866cae31738b8af6e16d26a914ae62
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2024-06-18 15:56:22 +1200

    feat: user profile that displays username and email

---
+++ project/src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+
+
+const Profile = () => {
+    const { isLoading, data: userData } = useQuery({
+        queryKey: ["userData"],
+        queryFn: () =>
+        fetch("https://s1-24-id608001-project-squigggggle.onrender.com/api/v1/user/current", {
+            headers: {
+                Authorization: `${localStorage.getItem("token")}`,
+            }
+        }
+        ).then((res) => res.json())
+    });
+    if (isLoading) return "Loading...";
+
+    return (
+        <>
+            <h1>Profile</h1>
+            <p>Username: {userData.data.username}</p>
+            <p>Email: {userData.data.email}</p>
+        </>
+    )
+}
+
+export default Profile;
