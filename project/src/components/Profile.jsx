3cfaabdd5a469c70d9c8a92c1cb30988367e0178
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2024-06-18 16:16:31 +1200

    feat: error checking profile page

--- project/src/components/Profile.jsx
+++ project/src/components/Profile.jsx
@@ -1,26 +1,33 @@
 import { useQuery } from "@tanstack/react-query";
 
-
 const Profile = () => {
-    const { isLoading, data: userData } = useQuery({
-        queryKey: ["userData"],
-        queryFn: () =>
-        fetch("https://s1-24-id608001-project-squigggggle.onrender.com/api/v1/user/current", {
-            headers: {
-                Authorization: `${localStorage.getItem("token")}`,
-            }
-        }
-        ).then((res) => res.json())
-    });
-    if (isLoading) return "Loading...";
+  const { isLoading, data: userData } = useQuery({
+    queryKey: ["userData"],
+    queryFn: () =>
+      fetch(
+        "https://s1-24-id608001-project-squigggggle.onrender.com/api/v1/user/current",
+        {
+          headers: {
+            Authorization: `${localStorage.getItem("token")}`,
+          },
+        },
+      ).then((res) => res.json()),
+  });
+  if (isLoading) return "Loading...";
 
-    return (
+  return (
+    <>
+      {userData.msg ? (
+        <div>{userData.msg}</div>
+      ) : (
         <>
-            <h1>Profile</h1>
-            <p>Username: {userData.data.username}</p>
-            <p>Email: {userData.data.email}</p>
+          <h1>Profile</h1>
+          <p>Username: {userData.data.username}</p>
+          <p>Email: {userData.data.email}</p>
         </>
-    )
-}
+      )}
+    </>
+  );
+};
 
 export default Profile;
