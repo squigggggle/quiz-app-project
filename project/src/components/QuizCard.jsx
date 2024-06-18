4672a669aaa68a662fbabc6d616b501bc66ee3ca
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2024-06-18 15:39:31 +1200

    feat: card with props to take quiz information

---
+++ project/src/components/QuizCard.jsx
@@ -0,0 +1,32 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+const QuizCard = (props) => {
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>{props.name}</CardTitle>
+          <CardDescription>Difficulty: {props.difficulty}</CardDescription>
+          <CardDescription>Type: {props.type}</CardDescription>
+        </CardHeader>
+        <CardContent>
+        </CardContent>
+        <CardFooter>
+            <ul>
+                <li>Starts: {props.startdate}</li>
+                <li>Ends: {props.enddate}</li>
+            </ul>
+        </CardFooter>
+      </Card>
+    </>
+  );
+};
+
+export default QuizCard;
