f1cc35a22e0cba8b2e9e4355c2135f2f15593237
squigggggle <younjo1@student.op.ac.nz>
2024-06-22 16:37:59 +1200

    chore: remove unused file

--- project/src/utils/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-// Create an axios instance
-const quizAPIInstance = axios.create({
-  baseURL: "https://s1-24-id608001-project-squigggggle.onrender.com", // Replace with your own API URL
-  timeout: 10000, // 10 seconds. Increase if requests are timing out
-});
-
-export { quizAPIInstance };
