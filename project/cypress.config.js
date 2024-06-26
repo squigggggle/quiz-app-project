da4ab261db5629b3a2686c73bf196fffc96d15a3
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2024-06-26 14:11:42 +1200

    test: add custom command to reset database before test run

---
+++ project/cypress.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from "cypress";
+import { exec } from 'child_process';
+import util from 'util';
+const execPromise = util.promisify(exec);
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      on('task', {
+        async resetDatabase() {
+          try {
+            const { stdout, stderr } = await execPromise('cd../ && npx prisma migrate reset --force && npx prisma db seed');
+            if (stderr) {
+              console.error('Error output:', stderr);
+            }
+            console.log('Reset output:', stdout);
+            return null;
+          } catch (error) {
+            console.error('Error resetting database:', error);
+            throw error;
+          }
+        }
+      });
+    },
+  },
+});
